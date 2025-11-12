e467ab1d3ab5e7fe527b8f13505a9d2d93e8bbb7
Maycon Mendes <mayconsouzamendes@gmail.com>
2025-11-12 07:01:26 -0300

    init: Clona o projeto starter e ajusta para rodar com npm

---
+++ README.md
@@ -0,0 +1,21 @@
+# Gallery Plus
+
+## Getting started
+
+Run the install command
+
+```
+npm install
+```
+
+Then in a terminal run the backend server
+
+```
+npm run dev-server
+```
+
+In another terminal run the frontend server
+
+```
+npm run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import Button from "./components/button.tsx";
+import ButtonIcon from "./components/button-icon.tsx";
+import ChevronLeftIcon from "./assets/icons/chevron-left.svg?react";
+import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
+import Badge from "./components/badge.tsx";
+import Alert from "./components/alert.tsx";
+import Divider from "./components/divider.tsx";
+
+export default function App() {
+	return (
+		<div className="grid gap-7 p-6">
+			<div className="flex gap-3">
+				<Button>Button</Button>
+				<Button variant="secondary">Button</Button>
+				<Button disabled>Button</Button>
+				<Button handling>Loading</Button>
+				<Button icon={ChevronRightIcon}>Próxima Imagem</Button>
+				<Button variant="ghost" size="sm">
+					Button
+				</Button>
+				<Button variant="primary" size="sm">
+					Button
+				</Button>
+			</div>
+
+			<div className="flex gap-3">
+				<ButtonIcon icon={ChevronLeftIcon} />
+				<ButtonIcon icon={ChevronRightIcon} variant="secondary" />
+			</div>
+
+			<div className="flex gap-3">
+				<Badge>Todos</Badge>
+				<Badge>Natureza</Badge>
+				<Badge>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+				<Badge loading>Viagem</Badge>
+			</div>
+
+			<div>
+				<Alert>
+					Tamanho máximo: 50MB
+					<br />
+					Você pode selecionar arquivos em PNG, JPG, JPEG ou WEBP
+				</Alert>
+			</div>
+
+			<div>
+				<Divider />
+			</div>
+		</div>
+	);
+}
